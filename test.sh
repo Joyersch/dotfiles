2a2b54132f981f6acfdd4de0b66f581055ea951b
Erik Schnittker <dev@joyersch.de>
2022-10-23 19:28:49 +0200

    Removed test.sh as it did not work as intended

--- test.sh
+++
@@ -1,7 +0,0 @@
-# all the lines from the readme
-pacman -Sy
-pacman -S --noconfirm git
-git clone https://github.com/Joyersch/dotfiles.git
-archinstall --silent --config dotfiles/user_configuration.json --creds dotfiles/user_credentials.json --disk_layouts dotfiles/user_disk_layout.json
-arch-chroot /mnt/archinstall/
-sh dotfiles/install.sh
