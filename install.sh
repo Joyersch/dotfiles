618266a06323dba8cd3d67dfd2b2005a73f3f479
Erik Schnittker <dev@joyersch.de>
2022-11-07 22:24:06 +0100

    sudo in script does not work :(

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 echo "THIS IS STILL WIP";
 
 # copy files from system folder
-sudo cp -p system/usr/share/backgrounds/* /usr/share/backgrounds/
+cp -p system/usr/share/backgrounds/* /usr/share/backgrounds/
+
 
 # configure openbox
 cp /etc/xdg/openbox/rc.xml .config/openbox/rc.xml
