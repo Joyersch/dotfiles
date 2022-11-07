85cc330b7ae37581aee3a0995c924d1e337870a8
Erik Schnittker <dev@joyersch.de>
2022-11-07 22:20:52 +0100

    Removed removing script files after executing

--- install.sh
+++ install.sh
@@ -1,22 +1,11 @@
 echo "THIS IS STILL WIP";
 
-#remove unessesary archinstall configration files
-rm user_configuration.json
-rm user_credentials.json
-rm user_disk_layout.json
-rm README.md
-rm LICENSE
-
-
 # copy files from system folder
-mkdir /usr/share/backgrounds/
-cp -p system/usr/share/backgrounds/* /usr/share/backgrounds/
+sudo cp -p system/usr/share/backgrounds/* /usr/share/backgrounds/
 
 # configure openbox
 cp /etc/xdg/openbox/rc.xml .config/openbox/rc.xml
 
-# remove system folder
-rm -r system
 
 # enable services
 systemctl enable sddm -f
