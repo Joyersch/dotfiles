dc496ce603efdcc237434a7a14b6fd57808939fe
Erik Schnittker <dev@joyersch.de>
2023-06-22 19:20:02 +0200

    Added alacritty themes

--- .config/openbox/themes/light.sh
+++ .config/openbox/themes/light.sh
@@ -1,7 +1,8 @@
 echo "source ~/.config/zsh/light" > ~/.config/zsh/theme
 nitrogen --set-auto ~/.backgrounds/light_pink.png
 xmlstarlet ed -L -N a="http://openbox.org/3.4/rc" -u '/a:openbox_config/a:theme/a:name' -v "light" ~/.config/openbox/rc.xml
-
+echo "import:" > ~/.config/alacritty/alacritty.yml
+echo "  - ~/.config/alacritty/themes/themes/ayu_light.yaml" >> ~/.config/alacritty/alacritty.yml
 #reload
 openbox --reconfigure
 . ~/.zshrc
--- install/cleanup.sh
+++ install/cleanup.sh
@@ -7,5 +7,8 @@ rm -r ~/.themes/dark.d
 cp -r ~/.themes/light.d/Leve-Lavender ~/.themes/light
 rm -r ~/.themes/light.d
 
+mkdir -p ~/.config/alacritty/themes
+git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
+
 # change shell to zsh
 chsh -s /usr/bin/zsh
