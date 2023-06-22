032bcd04bd02950f8fbf7ed0018fa17fa4d35020
Erik Schnittker <dev@joyersch.de>
2023-06-22 19:06:13 +0200

    updated order for theme scripts

--- .config/openbox/themes/dark.sh
+++ .config/openbox/themes/dark.sh
@@ -3,5 +3,5 @@ nitrogen --set-auto ~/.backgrounds/dark_purple.png
 xmlstarlet ed -L -N a="http://openbox.org/3.4/rc" -u '/a:openbox_config/a:theme/a:name' -v "dark" ~/.config/openbox/rc.xml
 
 #reload
-. ~/.zshrc
 openbox --reconfigure
+. ~/.zshrc
--- .config/openbox/themes/light.sh
+++ .config/openbox/themes/light.sh
@@ -3,5 +3,5 @@ nitrogen --set-auto ~/.backgrounds/light_pink.png
 xmlstarlet ed -L -N a="http://openbox.org/3.4/rc" -u '/a:openbox_config/a:theme/a:name' -v "light" ~/.config/openbox/rc.xml
 
 #reload
-. ~/.zshrc
 openbox --reconfigure
+. ~/.zshrc
