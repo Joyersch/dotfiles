12aec36040c3229df55c52b11816bb09c46dc0f5
Erik Schnittker <dev@joyersch.de>
2023-06-22 17:08:56 +0200

    Updated light and dark script files

--- .config/openbox/themes/dark.sh
+++ .config/openbox/themes/dark.sh
@@ -1,1 +1,5 @@
 echo "source ~/.config/zsh/dark" > ~/.config/zsh/theme
+nitrogen --set-auto ~/.backgrounds/dark_purple.png
+
+#reload
+. ~/.zshrc
--- .config/openbox/themes/light.sh
+++ .config/openbox/themes/light.sh
@@ -1,1 +1,4 @@
 echo "source ~/.config/zsh/light" > ~/.config/zsh/theme
+nitrogen --set-auto ~/.backgrounds/light_pink.png
+#reload
+. ~/.zshrc
